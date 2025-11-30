4d0519957d4b75603c5fb8e0aa0f5d38518bf0b8
pabitrabarua123 <pabitrabesus@gmail.com>
2025-11-30 20:18:24 +0530

    fix

--- src/app/checkout/success/page.tsx
+++ src/app/checkout/success/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useSearchParams } from "next/navigation";
 import CheckoutStatus from "@/components/CheckoutStatus/CheckoutStatus";
 
