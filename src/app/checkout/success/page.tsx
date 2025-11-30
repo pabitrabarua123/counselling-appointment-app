5e78d8b6fca6ae6616bdae9dcf11c6a9a15c24bb
pabitrabarua123 <pabitrabesus@gmail.com>
2025-11-30 19:09:12 +0530

    payment fix

--- src/app/checkout/success/page.tsx
+++ src/app/checkout/success/page.tsx
@@ -1,14 +1,18 @@
-import CheckoutStatus from '@/components/CheckoutStatus/CheckoutStatus';
+"use client";
 
-export default function SuccessPage({ searchParams }: { searchParams: { session_id?: string } }) {
-  const sessionId = searchParams.session_id || null;
+import { useSearchParams } from "next/navigation";
+import CheckoutStatus from "@/components/CheckoutStatus/CheckoutStatus";
+
+export default function SuccessPage() {
+  const searchParams = useSearchParams();
+  const sessionId = searchParams.get("session_id");
 
   if (!sessionId) {
     return (
       <div className="bg-gray-50 py-30">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-12">
-            <h1 className="text-4xl font-bold text-gray-900 mb-4">Invalid Page</h1>
+            <h1 className="text-4xl font-bold text-red-600 mb-4">Invalid Page</h1>
           </div>
         </div>
       </div>
