b9ec02281eaa9c8b720d84c370320307e8834cce
pabitrabarua123 <pabitrabesus@gmail.com>
2026-03-29 09:16:12 +0530

    remove report 1

--- src/app/admin/report/page.tsx
+++
@@ -1,9 +0,0 @@
-import Report from "@/components/Admin/pages/Report";
-
-export const metadata = {
-  title: "Admin Dashboard | TalkCure",
-};
-
-export default function AdminReport() {
-  return <Report />;
-}
